3f07c5833b88bfbf79efd1d15c59587f6d58e4c0
Michael Hahn <skipiano@gmail.com>
2021-08-30 14:33:54 -0700

    basic whack

---
+++ whack/app.js
@@ -0,0 +1,56 @@
+const square = document.querySelectorAll('.square')
+const mole = document.querySelectorAll('.mole')
+const timeLeft = document.querySelector('#time-left')
+let score = document.querySelector('#score')
+
+let result = 0
+let currentTime = timeLeft.textContent
+let hitPosition
+
+function randomSquare() {
+    square.forEach(className => {
+        className.classList.remove('mole')
+    })
+    let randomPos = square[Math.floor(Math.random() * 9)]
+    randomPos.classList.add('mole')
+
+
+    hitPosition = randomPos.id
+}
+
+square.forEach(id => {
+    id.addEventListener('mouseup', () => {
+        console.log("id: " + id.id)
+        console.log("hit: " + hitPosition)
+        if (id.id === hitPosition) {
+            result = result + 1
+            score.textContent = result
+        } else {
+            result = result - 0.3
+            score.textContent = result
+        }
+    })
+})
+
+
+function moveMole() {
+    randomSquare()
+    console.log(currentTime)
+    if (currentTime === 0) {
+        clearInterval(timerMove)
+    }
+}
+
+let timerMove = setInterval(moveMole, 1000)
+
+function countDown() {
+    currentTime--
+    timeLeft.textContent = currentTime
+
+    if (currentTime === 0) {
+        clearInterval(timerId)
+        alert('score: ' + result)
+    }
+}
+
+let timerId = setInterval(countDown, 1000)
